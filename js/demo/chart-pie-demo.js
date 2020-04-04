7f8495411fc8b00221945ad306636a2b143c55e9
ivan11cm <ivan11cm@gmail.com>
2020-04-04 16:08:10 +0200

    HEAD DESACOPLADO

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -4,10 +4,10 @@ Chart.defaults.global.defaultFontColor = '#858796';
 
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
-var testData = {"infected":[0,55,120,1000,10000,7700,6895,8000,6000,5500,3000,1090],"circulo":[500,23,123],"regions":{"cat":{
+var testData = {"infected":[0,55,120,1000,10000,7700,6895,8000,6000,5500,3000,1090],"circulo":[120,55,77],"regions":{"cat":{
   "population":{"x":[[4,5,6,7],[7,4,3,2]],
                 "y":[[4,5,6,7],[7,4,3,2]],   
-                "state":[[0,0,1,1],[]]}}}};
+                "state":[[0,0,1,1],[]]}}}}; 
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
